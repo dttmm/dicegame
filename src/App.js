da9a00f0e4f39f769d42683fdb05b69a13c26f12
dttmm <vkdnjxodnd@naver.com>
2021-07-27 21:08:43 +0900

    버튼 누를 때 마다 주사위 바뀜
    던지기 누르면 1~6
    처음부터 누르면 1

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import { useState } from "react";
 import Button from "./Button"
 import Dice from "./Dice"
 
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
+
 function App() {
   const [num, setNum] = useState(1);
 
   const handleRollClick = () =>{
-    setNum(3)
+    setNum(random(6))
+  }
+
+  const handleClearClick = () => {
+    setNum(1)
   }
 
   return (
     <div>
       <div>
         <Button onClick={handleRollClick}>던지기</Button>
-        <Button>처음부터</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
       </div>
       <Dice color="red" num={num}></Dice>
     </div>);
